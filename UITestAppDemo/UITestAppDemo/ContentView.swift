f94b98e60630cb81236f4b9334e4e22483f2b70a
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-09-12 06:44:46 +0600

    first test case added

--- UITestAppDemo/UITestAppDemo/ContentView.swift
+++ UITestAppDemo/UITestAppDemo/ContentView.swift
@@ -8,14 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showLoginView: Bool = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Good Morning!")
+                .font(.title)
+            Spacer().frame(height: 25)
+            Button {
+                showLoginView = true
+            } label: {
+                Text("Login")
+            }
+
         }
-        .padding()
     }
 }
 
