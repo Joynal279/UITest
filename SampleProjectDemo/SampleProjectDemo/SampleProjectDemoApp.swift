e6d9e5b762357d2a031c4cef076dd95375dc4549
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-09-13 22:34:09 +0600

    Development target changed 17 to 16

---
+++ SampleProjectDemo/SampleProjectDemo/SampleProjectDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleProjectDemoApp.swift
+//  SampleProjectDemo
+//
+//  Created by Joynal Abedin on 13/9/23.
+//
+
+import SwiftUI
+
+@main
+struct SampleProjectDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
